3a494fe016bb5ac5ab95673a489277f09fbd8f34
Anderson Murilo <anderson.xrilo@gmail.com>
2021-10-12 23:45:22 -0300

    meio do tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import { v4 as uuidv4 } from 'uuid';
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import "./css/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = ()=>{
+  const [tasks,setTasks] = useState(
+    [{
+      id: '1',
+      title: "Estudar Programação",
+      completed: false,
+    },{
+      id: "2",
+      title: "ler livros",
+      completed: true
+    }]
   );
+  const handleTaskAddition = (taskTitle) =>{
+    const newTasks = [...tasks,{
+      title: taskTitle,
+      id:uuidv4(),
+      completed:false
+    }]
+    setTasks(newTasks)
+  }
+  return (
+    <>
+      <div className="container">
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} />
+      </div>
+     
+    </>);
 }
 
 export default App;
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import "../css/AddTask.css";
+import Button from './Button';
+
+const AddTask = ({handleTaskAddition}) => {
+    const [inputData,setInputData] = useState('');
+    const handlerInputChange = (e)=>{
+            setInputData(e.target.value);
+    }
+    const handleAddTaskClick = () => {
+        if(inputData!=""){
+            handleTaskAddition(inputData);
+            setInputData("");
+        }else{alert("Adicione algum texto pelo ao menos"); }
+        
+    }
+    return ( 
+        <>
+        <div className="add-task-container">
+        <input onChange={handlerInputChange} value={inputData} type="text" className="add-task-input" />
+        <div className="add-task-button-container">
+        <Button onClick={handleAddTaskClick}>Adicionar</Button>
+        </div>
+        
+        </div>
+        
+        </>
+     );
+}
+ 
+export default AddTask;
